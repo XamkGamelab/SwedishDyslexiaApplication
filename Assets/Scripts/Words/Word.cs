9c785909cfa40275a8396f00c3acb98172d05128
Meeri Koskenkangas <fmeko004@edu.xamk.fi>
2025-05-07 10:48:19 +0300

    Added word class
    Also added the folder for it

---
+++ Assets/Scripts/Words/Word.cs
@@ -0,0 +1,9 @@
+namespace SwedishApp.Words
+{
+    [System.Serializable]
+    public class Word
+    {
+        public string swedishWord { get; private set; }
+        public string finnishWord { get; private set; }
+    }
+}
