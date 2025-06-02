8e48d50ae4cb5a45d70efd4fa122d96b18257589
Meeri Koskenkangas <fmeko004@edu.xamk.fi>
2025-06-02 14:44:11 +0300

    This doesn't work aaaa
    Okay so I managed to make everything look nice and assigned the values etc but now the slider doesn't do anything and the buttons don't make the music play aaaaaaaaa

--- Assets/Scripts/Core/AudioManager.cs
+++ Assets/Scripts/Core/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SwedishApp.Core
 {
@@ -18,26 +19,27 @@ namespace SwedishApp.Core
         }
 
         [Header("Music")]
-        [SerializeField] private AudioSource _menuMusic1;   // Not depressing I hope
-        [SerializeField] private AudioSource _menuMusic2;   // Dyslexiyass
-        [SerializeField] private AudioSource _menuMusic3;   // Chill bgm
-        [SerializeField] private AudioSource _gameMusic;    // Background sound
-        private bool _menuMusic1Playing = false;
-        private bool _menuMusic2Playing = false;
-        private bool _menuMusic3Playing = false;
-        private bool _buzzingPlaying = false;
-        private bool _gameMusicPlaying = false;
+        [SerializeField] private AudioSource menuMusic;
+        //private bool _menuMusic1Playing = false;
+        //private bool _menuMusic2Playing = false;
+        //private bool _menuMusic3Playing = false;
+        //private bool _buzzingPlaying = false;
+        //private bool _gameMusicPlaying = false;
 
         [Header("SFX")]
         [SerializeField] private AudioSource _menuSelect1;  // Beep
         [SerializeField] private AudioSource _menuSelect2;  // Bonk
         [SerializeField] private AudioSource _menuSelect3;  // Elevator
-        [SerializeField] private AudioSource _buzzing;
 
         [SerializeField] private AudioSource _lightModeToggle;
         [SerializeField] private AudioSource _inputSound;
         [SerializeField] private AudioSource _correct;
         [SerializeField] private AudioSource _incorrect;
+
+        [SerializeField] public Slider volumeSlider;
+
+        [Header("Audio settings")]
+        private readonly float musicVolume;
 
         private void Awake()
         {
@@ -51,134 +53,99 @@ namespace SwedishApp.Core
                 Debug.LogError($"Found more than one AudioManager, destroying duplicate. Fix this!");
             }
         }
-        public void StartMenuMusic1()
+
+        private void Start()
         {
-            if (!_menuMusic1Playing)
+            if(!PlayerPrefs.HasKey("musicVolume"))      // If there isn't previous saved values, set volume to default
             {
-                _menuMusic1.Play();
-                _menuMusic2.Stop();
-                _menuMusic3.Stop();
-                _gameMusic.Stop();
-                _buzzing.Stop();
-                _menuMusic1.volume = 1.0f;
-                _menuMusic1.pitch = 0.7f;
-                _menuMusic1Playing = true;
-                _menuMusic2Playing = false;
-                _menuMusic3Playing = false;
-                _gameMusicPlaying = false;
-                _buzzingPlaying = false;
+                PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
             }
-        }
-        public void StartMenuMusic2()  // Alternative music
-        {
-            if (!_menuMusic2Playing)
+            else
             {
-                _menuMusic1.Stop();
-                _menuMusic2.Play();
-                _menuMusic3.Stop();
-                _gameMusic.Stop();
-                _buzzing.Stop();
-                _menuMusic2.volume = 1.0f;
-                _menuMusic2.panStereo = -0.3f;
-                _menuMusic2.pitch = 1.0f;
-                _menuMusic1Playing = false;
-                _menuMusic2Playing = true;
-                _menuMusic3Playing = false;
-                _gameMusicPlaying = false;
-                _buzzingPlaying = false;
+                Load();
             }
         }
 
-        public void StartMenuMusic3()  // Alternative music
+        private void AudioSettings()
         {
-            if (!_menuMusic3Playing)
-            {
-                _menuMusic1.Stop();
-                _menuMusic2.Stop();
-                _menuMusic3.Play();
-                _gameMusic.Stop();
-                _buzzing.Stop();
-                _menuMusic3.volume = 1.0f;
-                _menuMusic3.panStereo = -0.3f;
-                _menuMusic3.pitch = 1.0f;
-                _menuMusic1Playing = false;
-                _menuMusic2Playing = false;
-                _menuMusic3Playing = true;
-                _gameMusicPlaying = false;
-                _buzzingPlaying = false;
-            }
+            //_menuMusic1.        volume      = musicVolume;
+            //_menuMusic1.        pitch       = 0.7f;
+
+            //_menuMusic2.        volume      = musicVolume;
+            //_menuMusic2.        panStereo   = -0.3f;
+            //_menuMusic2.        pitch       = 1.0f;
+
+            //_menuMusic3.        volume      = musicVolume;
+            //_menuMusic3.        panStereo   = -0.3f;
+            //_menuMusic3.        pitch       = 1.0f;
+
+            //_gameMusic.         volume      = musicVolume;
+            //_gameMusic.         pitch       = 0.9f;
+
+            //_buzzing.           volume      = 3.0f;
+            //_buzzing.           panStereo   = -0.25f;
+
+            _menuSelect1.       volume      = 1.0f;
+            _menuSelect2.       volume      = 2.0f;
+            _menuSelect3.       volume      = 0.05f;
+            _lightModeToggle.   volume      = 1.0f;
+            _lightModeToggle.   panStereo   = -0.6f;
+            _inputSound.        volume      = 1.0f;
+            _correct.           volume      = 0.5f;
+            _incorrect.         volume      = 1.0f;
         }
-        public void StartGameMusic()
+        public void ChangeVolume()
         {
-            if (!_gameMusicPlaying)
-            {
-                _menuMusic1.Stop();
-                _menuMusic2.Stop();
-                _menuMusic3.Stop();
-                _gameMusic.Play();
-                _buzzing.Stop();
-                _gameMusic.volume = 1.0f;
-                _gameMusic.pitch = 0.9f;
-                _menuMusic1Playing = false;
-                _menuMusic2Playing = false;
-                _menuMusic3Playing = false;
-                _gameMusicPlaying = true;
-                _buzzingPlaying = false;
-            }
+            AudioListener.volume = volumeSlider.value;
+            Save();
         }
-        public void StartBuzzing()  // Buzzing
+
+        private void Load()
         {
-            if (!_buzzingPlaying)
-            {
-                _menuMusic1.Stop();
-                _menuMusic2.Stop();
-                _menuMusic3.Stop();
-                _gameMusic.Stop();
-                _buzzing.Play();
-                _buzzing.volume = 3.0f;
-                _buzzing.panStereo = -0.25f;
-                _menuMusic1Playing = false;
-                _menuMusic2Playing = false;
-                _menuMusic3Playing = false;
-                _gameMusicPlaying = false;
-                _buzzingPlaying = true;
-            }
+            volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        }
+
+        private void Save()
+        {
+            PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        }
+
+        public void StartMusic(MusicData _musicData)
+        {
+            menuMusic.Stop();
+            menuMusic.clip = _musicData.audioClip;
+            menuMusic.volume = _musicData.volume * musicVolume;
+            menuMusic.pitch = _musicData.pitch;
+            menuMusic.panStereo = _musicData.pan;
+            menuMusic.Play();
         }
         public void PlayMenuSelect1()
         {
             _menuSelect1.Play();
-            _menuSelect1.volume = 1.0f;
         }
         public void PlayMenuSelect2()
         {
             _menuSelect2.Play();
-            _menuSelect2.volume = 1.0f;
         }
         public void PlayMenuSelect3()
         {
             _menuSelect3.Play();
-            _menuSelect3.volume = 0.25f;
         }
         public void PlayLightModeToggle()
         {
             _lightModeToggle.Play();
-            _lightModeToggle.volume = 1.0f;
-            _lightModeToggle.panStereo = -0.6f;
         }
         public void PlayInputSound()
         {
             _inputSound.Play();
-            _inputSound.volume = 1.0f;
         }
         public void PlayCorrect()
         {
             _correct.Play();
-            _correct.volume = 0.5f;
         }
         public void PlayIncorrect()
         {
             _incorrect.Play();
-            _incorrect.volume = 1.0f;
         }
     }
 }
---
+++ Assets/Scripts/Core/MusicData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SwedishApp.Core
+{
+    [System.Serializable]
+    public class MusicData : MonoBehaviour
+    {
+        public AudioClip audioClip;
+        public float volume = 1.0f;
+        public float pitch;
+        public float pan;
+    }
+}
