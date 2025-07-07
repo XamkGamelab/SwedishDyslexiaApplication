17fc98119b747c41119851dcb6df2907c05c1011
Meeri Koskenkangas <fmeko004@edu.xamk.fi>
2025-07-07 10:05:05 +0300

    Added month numbers
    -1 by default so Veera can do some black magic to not show it on non-month words

--- Assets/Scripts/Words/TimeWord.cs
+++ Assets/Scripts/Words/TimeWord.cs
@@ -1,5 +1,8 @@
 namespace SwedishApp.Words
 {
     [System.Serializable]
-    public class TimeWord : Word { }
+    public class TimeWord : Word 
+    {
+        public int monthNumber = -1;
+    }
 }
