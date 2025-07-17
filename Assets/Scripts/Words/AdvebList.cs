a4f098cf4157c4863d8af3583cf5b3980342256e
Meeri Koskenkangas <fmeko004@edu.xamk.fi>
2025-07-17 16:58:52 +0300

    Added a list for adverbs
    Also deleted some blank lines from the ends of some other code files

---
+++ Assets/Scripts/Words/AdvebList.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwedishApp.Words
+{
+    /// <summary>
+    /// This class simply houses a list of adverb-related words to be used in minigames.
+    /// </summary>
+    [CreateAssetMenu(menuName = "AdverbList")]
+    public class AdvebList : ScriptableObject
+    {
+        public List<AdverbWord> adverbList;
+    }
+}
--- Assets/Scripts/Words/GrammarWord.cs
+++ Assets/Scripts/Words/GrammarWord.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace SwedishApp.Words
 {
+    /// <summary>
+    /// This class houses the additional editor input field for notices
+    /// </summary>
     [System.Serializable]
     public class GrammarWord : Word
     {
