feaabfcd8c63ebecf3006adf75757031de19179d
Veera Raita <fvera003@edu.xamk.fi>
2025-08-28 22:01:50 +0300

    Removed any planned mobile support, needs more comprehensive rework.

--- Assets/Scripts/Input/MinigameInputField.cs
+++ Assets/Scripts/Input/MinigameInputField.cs
@@ -1,35 +1,17 @@
-using System.Diagnostics;
 using TMPro;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace SwedishApp.Input
 {
     public class MinigameInputField : TMP_InputField
     {
-        public override void OnSelect(BaseEventData eventData)
-        {
-            shouldHideSoftKeyboard = false;
-            var activeKeyboard = m_SoftKeyboard;
-            m_SoftKeyboard = null;
-            base.OnSelect(eventData);
-            m_SoftKeyboard = activeKeyboard;
-        }
-
-        public override void OnSubmit(BaseEventData eventData)
-        {
-            var activeKeyboard = m_SoftKeyboard;
-            m_SoftKeyboard = null;
-            base.OnSubmit(eventData);
-            m_SoftKeyboard = activeKeyboard;
-        }
-
+        //This is a remnant from trying to solve mobile keyboards disappearing
+        //after selecting a new input field via code. It is not possible to stop
+        //from happening in this way and you should refer instead to the method
+        //described in the handover documentation to avoid repeating my folly.
         public override void OnDeselect(BaseEventData eventData)
         {
-            var activeKeyboard = m_SoftKeyboard;
-            m_SoftKeyboard = null;
             base.OnDeselect(eventData);
-            m_SoftKeyboard = activeKeyboard;
         }
     }
 }
